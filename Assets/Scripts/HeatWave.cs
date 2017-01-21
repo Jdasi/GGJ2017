1823078d0eea711d83e4a4a7af75adc5c27ef061
Joseph da Silva <joseph2.dasilva@live.uwe.ac.uk>
2017-01-21 02:42:18 +0000

    Added tile switching based on heatwave

---
+++ Assets/Scripts/HeatWave.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeatWave : MonoBehaviour
+{
+    public bool active = false;
+
+    Tile[] tiles;
+
+	void Start()
+    {
+        tiles = GameObject.FindObjectsOfType<Tile>();
+	}
+	
+	void Update()
+    {
+        if (Input.GetButtonDown("HeatWave"))
+        {   
+            active = !active;
+
+            foreach (Tile tile in tiles)
+            {
+                tile.transition();
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour
+{
+    // TEMP STUFF WHILE WE WAIT FOR ART
+    public Color cold_color = Color.cyan;
+    public Color hot_color = Color.red;
+
+    public bool visible_in_cold = true;
+    public bool visible_in_hot = true;
+
+    private HeatWave heat_wave;
+    private SpriteRenderer sprite;
+
+	void Start()
+    {
+        heat_wave = GameObject.Find("HeatWaveManager").GetComponent<HeatWave>();
+        sprite = GetComponent<SpriteRenderer>();
+
+        transition();
+	}
+	
+	void Update ()
+    {
+
+	}
+
+    public void transition()
+    {
+        if (!heat_wave.active && visible_in_cold)
+            gameObject.SetActive(true);
+        else if (heat_wave.active && visible_in_hot)
+            gameObject.SetActive(true);
+        else
+            gameObject.SetActive(false);
+
+        sprite.color = heat_wave.active ? hot_color : cold_color;
+    }
+}
