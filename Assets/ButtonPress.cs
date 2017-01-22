a6460009ad434ac557e71794d86241c2c8322509
Elliott Smith <Elliott2.Smith@live.uwe.ac.uk>
2017-01-22 03:51:57 +0000

    Replaced drawbridge with platform

--- Assets/ButtonPress.cs
+++ Assets/ButtonPress.cs
@@ -4,55 +4,88 @@ using System.Collections;
 public class ButtonPress : MonoBehaviour {
     public bool dropBridge = false;
     public bool entered = false;
-    private GameObject bridge;
+    private GameObject platform;
     Vector3 rotationEuler;
     private float currentTime = 0;
+    bool moveDirection = true;
+    bool move = false;
 
 	void Start () {
-        bridge = GameObject.Find("Drawbridge");
+        platform = GameObject.Find("Platform");
 	}
 
 
     void Update () {
+
+        if ((platform.transform.position.y < 2) || (platform.transform.position.y > 7))
+        {
+            if (moveDirection)
+            {
+                moveDirection = false;
+            }
+
+            else
+            {
+                moveDirection = true;
+            }
+        }
+
+        if (move)
+        {
+            if (platform.transform.position.y > 2)
+            {
+                platform.transform.position += Vector3.down * 1 * Time.deltaTime;
+            }     
+        }
+
+        else
+        {
+            if (platform.transform.position.y < 7)
+            {
+                platform.transform.position += Vector3.down * -1 * Time.deltaTime;
+            }
+        }
+
         //if player on trigger and not fully dropped
         //print(transform.rotation.eulerAngles);
-        if (dropBridge)
-        {
-            if (currentTime <= 3)
-            {
-                currentTime += Time.deltaTime;
+        /* if (dropBridge)
+         {
+             if (currentTime <= 3)
+             {
+                 currentTime += Time.deltaTime;
 
-                rotationEuler -= Vector3.forward * 30 * Time.deltaTime; //increment 30 degrees every second
-                bridge.transform.rotation = Quaternion.Euler(rotationEuler);
-                Debug.Log(currentTime);
-            }
-        }
-        else if (!dropBridge && entered)
-        {
-            if (currentTime <= 3)
-            {
-                Debug.Log(currentTime);
-                currentTime += Time.deltaTime;
-                rotationEuler += Vector3.forward * 31 * Time.deltaTime; //increment 30 degrees every second
-                bridge.transform.rotation = Quaternion.Euler(rotationEuler);
-            }
-        }
-
+                 rotationEuler -= Vector3.forward * 30 * Time.deltaTime; //increment 30 degrees every second
+                 bridge.transform.rotation = Quaternion.Euler(rotationEuler);
+                 Debug.Log(currentTime);
+             }
+         }
+         else if (!dropBridge && entered)
+         {
+             if (currentTime <= 3)
+             {
+                 Debug.Log(currentTime);
+                 currentTime += Time.deltaTime;
+                 rotationEuler += Vector3.forward * 31 * Time.deltaTime; //increment 30 degrees every second
+                 bridge.transform.rotation = Quaternion.Euler(rotationEuler);
+             }
+         }*/
     }
 
     void OnTriggerEnter2D()
     {
-        currentTime = 0;
+        //currentTime = 0;
     }
     void OnTriggerStay2D()
     {
-        entered = true;
-        dropBridge = true;
+        move = true;
+        //entered = true;
+        //dropBridge = true;
     }
 
     void OnTriggerExit2D()
     {
-        currentTime = 0;
-        dropBridge = false;
+        move = false;
+        //currentTime = 0;
+        //dropBridge = false;
     }
 }
