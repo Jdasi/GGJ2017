316782956096700cb22520e639f951d87dcb777b
callan-patay <Callan2.Patay@live.uwe.ac.uk>
2017-01-22 04:04:37 +0000

    Menu complete mothafuckaz (except for title)
    gey

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,14 +3,54 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
+
+    private Animator animation;
+    private bool instructionsOpen = false;
+
+
+    void Start()
+    {
+        animation = GetComponent<Animator>();
+    }
+
     public void StartGame()
     {
-        Application.LoadLevel("Main");
+        if(!instructionsOpen)
+        {
+            Application.LoadLevel("Main");
+        }
+
     }
 
 
     public void QuitGame()
     {
-        Application.Quit();
+        if (!instructionsOpen)
+        {
+            Application.Quit();
+        }
     }
+
+
+    public void toggleInstructions()
+    {
+        instructionsOpen = !instructionsOpen;
+
+
+        if (instructionsOpen)
+        {
+            animation.SetTrigger("Showing");
+        }
+        else if (!instructionsOpen)
+        {
+            animation.SetTrigger("Off");
+        }
+
+
+       
+    }
+
+
+
+
 }
