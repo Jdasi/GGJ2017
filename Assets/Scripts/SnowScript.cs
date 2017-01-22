7d8ffe7918b7dd61ffaa38741b8585f3aae198a9
Elliott Smith <Elliott2.Smith@live.uwe.ac.uk>
2017-01-22 02:05:26 +0000

    now with SNOW

---
+++ Assets/Scripts/SnowScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnowScript : MonoBehaviour {
+
+    private WorldState world_state;
+
+    // Use this for initialization
+    void Start () {
+        world_state = GameObject.Find("WorldStateManager").GetComponent<WorldState>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!world_state.is_hot)
+        {
+            GetComponent<ParticleSystem>().enableEmission = true;
+        }
+
+        else
+        {
+            GetComponent<ParticleSystem>().enableEmission = false;
+        }
+    }
+}
